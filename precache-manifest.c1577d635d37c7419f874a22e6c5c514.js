732fc0cc03f2cb026c367a2b0431616423260485
Maloney L <maloneyl@users.noreply.github.com>
2018-11-05 19:47:29 +0000

    Updates

---
+++ precache-manifest.c1577d635d37c7419f874a22e6c5c514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux-pwa/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "987942c955a319388140",
+    "url": "/robofriends-redux-pwa/static/js/runtime~main.987942c9.js"
+  },
+  {
+    "revision": "1123c79dc319247f5ae1",
+    "url": "/robofriends-redux-pwa/static/js/main.1123c79d.chunk.js"
+  },
+  {
+    "revision": "6a25de2d3df3e1b86824",
+    "url": "/robofriends-redux-pwa/static/js/1.6a25de2d.chunk.js"
+  },
+  {
+    "revision": "1123c79dc319247f5ae1",
+    "url": "/robofriends-redux-pwa/static/css/main.e9b282c7.chunk.css"
+  },
+  {
+    "revision": "6a25de2d3df3e1b86824",
+    "url": "/robofriends-redux-pwa/static/css/1.d544a567.chunk.css"
+  },
+  {
+    "revision": "97913f4fb4fd407f1e1d025a7813f574",
+    "url": "/robofriends-redux-pwa/index.html"
+  }
+];
