83ebf5d3378c1aee96525cad90d8e64acdccfa34
Maloney L <maloneyl@users.noreply.github.com>
2018-11-23 16:57:36 -0500

    use snapshot testing for Card component

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,7 +1,12 @@
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Card from './Card';
 
 it('expects to render Card component', () => {
   expect(shallow(<Card />).length).toEqual(1)
+})
+
+// snapshot testing
+it('expects to render Card component', () => {
+  expect(shallow(<Card />)).toMatchSnapshot();
 })
