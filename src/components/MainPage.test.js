cf99628cf2a302dc049522ee0ce90d0d953a47a1
Maloney L <maloneyl@users.noreply.github.com>
2018-11-24 18:05:04 -0500

    simplify App so it's all redux while MainPage gets its own component (and tests)

---
+++ src/components/MainPage.test.js
@@ -0,0 +1,45 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import MainPage from './MainPage';
+
+const mockProps = {
+  onRequestRobots: jest.fn(),
+  robots: [],
+  searchField: '',
+  isPending: false
+}
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<MainPage { ...mockProps } />);
+})
+
+it('renders MainPage', () => {
+  expect(wrapper).toMatchSnapshot();
+})
+
+it('filters robots correctly', () => {
+  expect(wrapper.instance().filterRobots()).toEqual([])
+
+  const mockRobot = {
+    id: 3,
+    name: 'Jane',
+    email: 'jane@email.com'
+  }
+
+  const mockProps2 = {
+    ...mockProps,
+    robots: [mockRobot],
+    searchField: mockRobot.name.substring(0, 2)
+  }
+  const wrapper2 = shallow(<MainPage {...mockProps2 } />);
+  expect(wrapper2.instance().filterRobots()).toEqual([mockRobot]);
+
+  const mockProps3 = {
+    ...mockProps2,
+    searchField: 'nope'
+  }
+  const wrapper3 = shallow(<MainPage {...mockProps3 } />);
+  expect(wrapper3.instance().filterRobots()).toEqual([]);
+})
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setSearchField, requestRobots } from '../actions';
 
-import Header from '../components/Header.js';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
+import MainPage from '../components/MainPage';
 
 import './App.css';
 
@@ -29,28 +25,8 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-  componentDidMount() {
-    this.props.onRequestRobots();
-  }
-
   render() {
-    const { robots, searchField, onSearchChange, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    })
-    return (
-      <div className='tc'>
-        <Header />
-        <SearchBox searchChange={onSearchChange}/>
-        <Scroll>
-          { isPending ? <h1>Loading</h1> :
-            <ErrorBoundary>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundary>
-          }
-        </Scroll>
-      </div>
-    );
+    return <MainPage { ...this.props }/>
   }
 }
 
