bf03152f952540f447337b62f3f95d5bed621e04
Maloney L <maloneyl@users.noreply.github.com>
2019-01-03 20:54:54 +0000

    set up mock store and test requestRobots action

--- src/actions.test.js
+++ src/actions.test.js
@@ -7,6 +7,13 @@ import {
   REQUEST_ROBOTS_FAILED
  } from './constants';
 
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+
+// you might have a setup file
+// and export the mockStore for all your tests
+const mockStore = configureMockStore([thunkMiddleware]);
+
 describe('setSearchField', () => {
   it('creates an action to search robots', () => {
     const text = 'hello';
@@ -16,5 +23,19 @@ describe('setSearchField', () => {
     }
 
     expect(actions.setSearchField(text)).toEqual(expectedAction);
+  })
+})
+
+describe('requestRobots', () => {
+  it('handles the request robots API', () => {
+    const store = mockStore();
+    store.dispatch(actions.requestRobots());
+    const actions = store.getActions();
+
+    const expectedAction = {
+      type: REQUEST_ROBOTS_PENDING,
+    }
+
+    expect(actions[0]).toEqual(expectedAction)
   })
 })
